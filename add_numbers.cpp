2d06f249a3ed86887db2506bc493381900c65113
Joshua Manaol <jmanaol@zetaglobal.com>
2022-09-25 00:28:34 +0800

    edit filename

--- add_numbers.cpp
+++ add_numbers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//TODO make the function compute in other operations
 int addNumbers ( int first_param, int second_param){
     int result = first_param + second_param;
     return result;
